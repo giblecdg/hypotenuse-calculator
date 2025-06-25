0947cd9b2e8d7b3a8d6e1ea8ad24b211fbd79640
giblecdg <giblecdg@gmail.com>
2025-06-25 20:41:46 +0200

    Add project files.

---
+++ hypotenuse-calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hypotenuse_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side a: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter side b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine($"c = {c}");
+        }
+    }
+}
